2dc3c93db557036dd57a8bf38d85bae73b7d33e0
cmoore <cmooore@captechventures.com>
2017-10-30 16:37:45 -0400

    first commit

---
+++ src/com/pluralsight/model/Customer.java
@@ -0,0 +1,28 @@
+package com.pluralsight.model;
+
+public class Customer {
+	
+	private String firstname1;
+	private String lastname;
+	
+	public Customer() {
+		
+	}
+
+	public String getFirstname1() {
+		return firstname1;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setFirstname1(String firstname1) {
+		this.firstname1 = firstname1;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
